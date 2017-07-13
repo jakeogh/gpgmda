0146e97d569c95b95667b46cacf72c8aafe52944
jakeogh <git@v6y.net>
2017-07-12 22:11:03 -0700

    better installer

--- debian_installer.sh
+++ debian_installer.sh
@@ -6,7 +6,7 @@
 #scp ~/cfg/_myapps/gpgmda/check_postfix_config root@mail.domain.net:/root/check_postfix_config || exit 1
 #scp ~/cfg/_myapps/gpgmda/aliases root@mail.domain.net:/etc/aliases || exit 1
 
-exit 1 # comment this out to run on postfix server
+#exit 1 # comment this out to run on postfix server
 
 apt-get -y update || exit 1
 apt-get -y upgrade || exit 1
@@ -16,17 +16,21 @@ apt-get -y install nano || exit 1
 apt-get -y install htop || exit 1
 apt-get -y install mc || exit 1
 apt-get -y install postfix || exit 1
+apt-get -y install postfix-pcre || exit 1
+apt-get -y install postfix-policyd-spf-python || exit 1
 apt-get -y install rsync || exit 1
 apt-get -y install rdiff-backup || exit 1
-groupadd mailreaders || exit 1
-usermod -a -G mailreaders root || exit 1
-adduser user || exit 1
-adduser sentuser || exit 1
+id -u user >/dev/null 2>&1 || adduser user
+id -u sentuser >/dev/null 2>&1 || adduser sentuser
+[ $(getent group mailreaders) ] || { groupadd mailreaders || exit 1 ; }
+groups root | grep mailreaders || { usermod -a -G mailreaders root && exec su -l $USER || exit 1 ; }
+groups user | grep mailreaders || { usermod -g mailreaders user || exit 1 ; }
+groups sentuser | grep mailreaders || { usermod -g mailreaders sentuser || exit 1 ; }
 #copy .gnupg to /home/user and /home/sentuser
 chown -R user:user /home/user || exit 1
 chown -R sentuser:sentuser /home/sentuser || exit 1
-/root/check_postfix_config user@v6y.net sentuser@v6y.net #makes folders, expect failure here due to missing symlink
-ln -sf /home/sentuser/gpgMaildir/new /home/user/gpgMaildir/.sent || exit 1
+./check_postfix_config user@v6y.net sentuser@v6y.net #makes folders, expect failure here due to missing symlink
+test -h /home/user/gpgMaildir/.sent || { ln -sf /home/sentuser/gpgMaildir/new /home/user/gpgMaildir/.sent || exit 1 ; }
 newaliases
 
 
